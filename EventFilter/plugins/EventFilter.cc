719728baffb2c0453b60295afcb9b017485d2f02
Frici <fnemes@cern.ch>
2023-07-04 12:18:55 +0200

    Event filter

---
+++ EventFilter/plugins/EventFilter.cc
@@ -0,0 +1,97 @@
+#include <memory>
+#include <iostream>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/stream/EDFilter.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/StreamID.h"
+
+#include "DataFormats/CTPPSReco/interface/CTPPSLocalTrackLite.h"
+#include "DataFormats/CTPPSReco/interface/TotemRPLocalTrack.h"
+
+
+//
+// class declaration
+//
+
+using namespace std;
+
+class EventFilter : public edm::stream::EDFilter<> {
+public:
+  explicit EventFilter(const edm::ParameterSet&);
+  ~EventFilter() override;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+private:
+
+  void beginStream(edm::StreamID) override;
+  bool filter(edm::Event&, const edm::EventSetup&) override;
+  void endStream() override;
+
+  edm::EDGetTokenT<std::vector<CTPPSLocalTrackLite>> tracksToken_;
+  edm::EDGetTokenT<edm::DetSetVector<TotemRPLocalTrack>> tracksToken2_;
+
+};
+
+EventFilter::EventFilter(const edm::ParameterSet& iConfig) : tracksToken_(consumes<std::vector<CTPPSLocalTrackLite>>(iConfig.getUntrackedParameter<edm::InputTag>("tracks"))),
+																							tracksToken2_(consumes<edm::DetSetVector<TotemRPLocalTrack>>(iConfig.getUntrackedParameter<edm::InputTag>("tracks2")))
+{
+}
+
+EventFilter::~EventFilter()
+{
+}
+
+
+bool EventFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+
+	bool status = false ;
+
+  using namespace edm;
+
+  EventNumber_t event_number = iEvent.id().event() ;
+  RunNumber_t run_number = iEvent.run() ;
+
+  for(const auto& track : iEvent.get(tracksToken_))
+  {
+	   status = true ;
+  }
+
+  for(const auto& track : iEvent.get(tracksToken2_))
+  {
+	  for(const auto& track2 : track)
+	  {
+		status = true ;	  
+	  }
+  }
+  
+  // if(status) cout << "RP activity saved" << endl ;
+  // else
+  // cout << "RP no activity" << endl ;
+
+  return status;
+}
+
+void EventFilter::beginStream(edm::StreamID)
+{
+}
+
+void EventFilter::endStream()
+{
+}
+
+void EventFilter::fillDescriptions(edm::ConfigurationDescriptions& descriptions)
+{
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+DEFINE_FWK_MODULE(EventFilter);
---
+++ EventFilter/test/cfg_filter_reduction.py
@@ -0,0 +1,91 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("EventFilter")
+
+# minimum of logs
+process.MessageLogger = cms.Service("MessageLogger",
+  statistics = cms.untracked.vstring(),
+  destinations = cms.untracked.vstring('cout'),
+  cout = cms.untracked.PSet(
+    threshold = cms.untracked.string('WARNING')
+  )
+)
+
+#process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+# process.GlobalTag.globaltag = "101X_dataRun2_Express_v8"
+process.GlobalTag.globaltag = "101X_dataRun2_Prompt_v11"
+
+process.content = cms.EDFilter("EventContentFilter")
+
+process.maxEvents = cms.untracked.PSet(
+  input = cms.untracked.int32(-1)
+)
+
+process.source = cms.Source("PoolSource",
+  dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
+  inputCommands = cms.untracked.vstring(
+    'drop *',
+	 'keep TotemTriggerCounters_totemTriggerRawToDigi_*_*',
+	 'keep TotemFEDInfos_totemRPRawToDigi_*_*',
+	 'keep TotemRPDigiedmDetSetVector_totemRPRawToDigi_*_*',
+	 'keep TotemVFATStatusedmDetSetVector_totemRPRawToDigi_*_*',
+	 'keep TotemRPClusteredmDetSetVector_totemRPClusterProducer_*_*',
+	 'keep TotemRPRecHitedmDetSetVector_totemRPRecHitProducer_*_*',
+	 'keep TotemRPUVPatternedmDetSetVector_totemRPUVPatternFinder_*_*',
+	 'keep TotemRPLocalTrackedmDetSetVector_totemRPLocalTrackFitter_*_*',
+	 'keep TotemFEDInfos_totemT2Digis_*_*',
+	 'keep TotemT2DigiedmNewDetSetVector_totemT2Digis_*_*',
+	 'keep TotemVFATStatusedmDetSetVector_totemT2Digis_*_*',
+	 'keep TotemT2RecHitedmNewDetSetVector_totemT2RecHits_*_*',
+	 'keep TotemFEDInfos_ctppsDiamondRawToDigi_*_*',
+	 'keep CTPPSDiamondDigiedmDetSetVector_ctppsDiamondRawToDigi_*_*',
+	 'keep TotemVFATStatusedmDetSetVector_ctppsDiamondRawToDigi_*_*',
+	 'keep CTPPSDiamondRecHitedmDetSetVector_ctppsDiamondRecHits_*_*',
+	 'keep CTPPSDiamondLocalTrackedmDetSetVector_ctppsDiamondLocalTracks_*_*',
+	 'keep TotemTimingLocalTrackedmDetSetVector_diamondSampicLocalTracks_*_*',
+	 'keep TotemTimingDigiedmDetSetVector_totemTimingRawToDigi_*_*',
+	 'keep TotemTimingRecHitedmDetSetVector_totemTimingRecHits_*_*',
+	 'keep TotemTimingLocalTrackedmDetSetVector_totemTimingLocalTracks_*_*',
+	 'keep CTPPSPixelDigiedmDetSetVector_ctppsPixelDigis_*_*',
+	 'keep CTPPSPixelDataErroredmDetSetVector_ctppsPixelDigis_*_*',
+	 'keep CTPPSPixelClusteredmDetSetVector_ctppsPixelClusters_*_*',
+	 'keep CTPPSPixelRecHitedmDetSetVector_ctppsPixelRecHits_*_*',
+	 'keep CTPPSPixelLocalTrackedmDetSetVector_ctppsPixelLocalTracks_*_*',
+	 'keep CTPPSLocalTrackLites_ctppsLocalTrackLiteProducer_*_*',
+	 'keep recoForwardProtons_ctppsProtons_*_*',
+  ),
+  fileNames = cms.untracked.vstring(
+"/store/data/Run2023C/ZeroBias11/AOD/PromptReco-v4/000/369/585/00000/dbd5bb7f-2ab6-4613-aedc-c671ba4f467d.root",
+)
+)
+
+process.filter = cms.EDFilter("EventFilter",
+  verbosity = cms.untracked.int32(0),
+  tracks = cms.untracked.InputTag('ctppsLocalTrackLiteProducer'),
+  tracks2 = cms.untracked.InputTag('totemRPLocalTrackFitter'),
+  rpPatternTag = cms.InputTag('totemRPUVPatternFinder'),
+  outputFileName = cms.string("output.root"),
+)
+
+process.path_filter = cms.Path(process.filter)
+
+# output configuration
+process.output = cms.OutputModule("PoolOutputModule",
+  fileName = cms.untracked.string("output_skimming.root"),
+
+  SelectEvents = cms.untracked.PSet(
+    SelectEvents = cms.vstring('path_filter')
+  )
+)
+
+process.path_output = cms.EndPath(process.output)
+
+# process.path_output = cms.EndPath(process.output)
+# process.schedule = cms.Schedule(process.path_filter, process.path_output)
+
+
+
