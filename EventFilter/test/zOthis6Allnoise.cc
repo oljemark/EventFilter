1d1b41753ea13ff11257b88001d5c2bab6ff00e3
Fredrik <fredrik.oljemark@helsinki.fi>
2023-10-25 14:57:17 +0200

    Channel combination efficiency plots with 2-sigma cut

---
+++ EventFilter/test/zOthis6Allnoise.cc
@@ -0,0 +1,74 @@
+{
+ TH1D* Eff24=new TH1D("Eff24",">=2/4 Plane efficiency in each wedge w/o noise cut;Wedge number;Efficiency",16,-0.5,15.5);
+ TH1D* Eff34=new TH1D("Eff34",">=3/4 Plane efficiency in each wedge w/o noise cut;Wedge number;Efficiency",16,-0.5,15.5);
+ TH1D* Eff44=new TH1D("Eff44","4/4 Plane efficiency in each wedge w/o noise cut;Wedge number;Efficiency",16,-0.5,15.5);
+const double pl3[16]={0.97635605,0.182640145,0.866666667,0.939297125,0.916918429,0.87687188,0.973333333,0.242658423,0.911042945,0.970266041,0.466055046,0.851590106,0.816546763,0.939306358,0.702702703,0.657097289};
+const double er3[16]={0.005810472,0.016440326,0.015091149,0.009630549,0.010784355,0.013439163,0.011474289,0.016846231,0.015916956,0.006867114,0.021329528,0.014967911,0.016423802,0.01304527,0.022622323,0.018933307};
+const double pl2[16]={0.983193277,0.952830189,0.9844098,0.897709924,0.,0.963436929,0.977678571,0.98125,0.512953368,0.906432749,0.951310861,0.981670061,0.974248927,0.855263158,0.982817869,0.988009592};
+const double er2[16]={0.004989767,0.022042391,0.006215601,0.011884052,0.001611604,0.008185945,0.010693714,0.012192543,0.020736612,0.01118193,0.013542807,0.006348982,0.007596431,0.018097111,0.008274041,0.005804046};
+const double pl1[16]={0.882352941,0.971698113,0.812917595,0.986259542,0.982200647,0.972577697,0.9375,0.71875,0.91537133,0.964912281,0.981273408,0.928716904,0.984978541,0.923684211,0.982817869,0.93764988};
+const double er1[16]={0.012088016,0.018173127,0.018415616,0.004776443,0.005532933,0.007179031,0.016558784,0.035427757,0.011635062,0.007154594,0.009003846,0.011717632,0.005991784,0.013763802,0.008274041,0.011995134};
+const double pl0[16]={0.930576071,0.927927928,0.778251599,0.888583219,0.8395574,0.840442338,0.352941176,0.942622951,0.907534247,0.925666199,0.894197952,0.829090909,0.853159851,0.676300578,0.739018088,0.876681614};
+const double er0[16]={0.009836285,0.025470488,0.019178066,0.011702218,0.01366275,0.014571479,0.019564736,0.022062544,0.012047108,0.009881866,0.018105992,0.016066157,0.015287414,0.020509681,0.022300678,0.015625039};
+
+
+ for (int i=0;i<16;i++)
+ {
+	 const auto eff4=pl0[i]*pl1[i]*pl2[i]*pl3[i];
+	 const auto n0=1.-pl0[i];
+	 const auto n1=1.-pl1[i];
+	 const auto n2=1.-pl2[i];
+	 const auto n3=1.-pl3[i];
+	 auto eff3=n0*pl1[i]*pl2[i]*pl3[i];
+	 eff3+=pl0[i]*n1*pl2[i]*pl3[i];
+	 eff3+=pl0[i]*pl1[i]*n2*pl3[i];
+	 eff3+=pl0[i]*pl1[i]*pl2[i]*n3;
+	 auto eff2=n0*n1*pl2[i]*pl3[i];// 0011 + 1100 + 1010 + 1001 + 0101 + 0110
+	 eff2+=pl0[i]*pl1[i]*n2*n3;
+	 eff2+=pl0[i]*n1*pl2[i]*n3;
+	 eff2+=pl0[i]*n1*n2*pl3[i];
+	 eff2+=n0*pl1[i]*n2*pl3[i];
+	 eff2+=n0*pl1[i]*pl2[i]*n3;
+	 const auto e30=er0[i]*(pl1[i]*pl2[i]+pl1[i]*pl3[i]+pl2[i]*pl3[i]-3*pl1[i]*pl2[i]*pl3[i]);
+	 const auto e31=er1[i]*(pl0[i]*pl2[i]+pl0[i]*pl3[i]+pl2[i]*pl3[i]-3*pl0[i]*pl2[i]*pl3[i]);
+	 const auto e32=er2[i]*(pl0[i]*pl1[i]+pl0[i]*pl3[i]+pl1[i]*pl3[i]-3*pl0[i]*pl1[i]*pl3[i]);
+	 const auto e33=er3[i]*(pl0[i]*pl1[i]+pl0[i]*pl2[i]+pl1[i]*pl2[i]-3*pl0[i]*pl1[i]*pl2[i]);
+	 const auto erreff3=sqrt(e30*e30+e31*e31+e32*e32+e33*e33);
+	 const auto e40=er0[i]/pl0[i];
+	 const auto e41=er1[i]/pl1[i];
+	 const auto e42=er2[i]/(pl2[i]>0.000001 ? pl2[i] : 0.001);
+	 const auto e43=er3[i]/pl3[i];
+	 auto erreff4=eff4*sqrt(e40*e40+e41*e41+e42*e42+e43*e43);
+	 if (i==4) 
+		 erreff4=pl0[i]*pl1[i]*pl3[i]*er2[i];
+	 const auto e20=er0[i]*(3*pl1[i]*pl2[i]*pl3[i]+pl1[i]+pl2[i]+pl3[i]-2*pl1[i]*pl2[i]-2*pl1[i]*pl3[i]-2*pl2[i]*pl3[i]);
+	 const auto e21=er1[i]*(3*pl0[i]*pl2[i]*pl3[i]+pl0[i]+pl2[i]+pl3[i]-2*pl0[i]*pl2[i]-2*pl0[i]*pl3[i]-2*pl2[i]*pl3[i]);
+	 const auto e22=er2[i]*(3*pl0[i]*pl1[i]*pl3[i]+pl0[i]+pl1[i]+pl3[i]-2*pl0[i]*pl1[i]-2*pl0[i]*pl3[i]-2*pl1[i]*pl3[i]);
+	 const auto e23=er3[i]*(3*pl0[i]*pl1[i]*pl2[i]+pl0[i]+pl1[i]+pl2[i]-2*pl0[i]*pl1[i]-2*pl0[i]*pl2[i]-2*pl1[i]*pl2[i]);
+	 const auto erreff2=sqrt(e20*e20+e21*e21+e22*e22+e23*e23);
+	 Eff24->SetBinContent(i+1,eff2+eff3+eff4);
+	 Eff24->SetBinError(i+1,erreff2);
+	 Eff34->SetBinContent(i+1,eff3+eff4);
+	 Eff34->SetBinError(i+1,erreff3);
+	 Eff44->SetBinContent(i+1,eff4);
+	 Eff44->SetBinError(i+1,erreff4);
+ }
+
+TCanvas MyC;
+gStyle->SetOptStat(0);
+Eff24->GetYaxis()->SetRangeUser(0.,1.01);
+Eff24->Draw();
+MyC.Print("TwoOfFourPlaneEfficiencies-v8noNoiseC.pdf");
+MyC.Print("TwoOfFourPlaneEfficiencies-v8noNoiseC.png");
+MyC.Print("TwoOfFourPlaneEfficiencies-v8noNoiseC.C");
+Eff34->GetYaxis()->SetRangeUser(0.,1.01);
+Eff34->Draw();
+MyC.Print("ThreeOfFourPlaneEfficiencies-v8noNoiseC.pdf");
+MyC.Print("ThreeOfFourPlaneEfficiencies-v8noNoiseC.png");
+MyC.Print("ThreeOfFourPlaneEfficiencies-v8noNoiseC.C");
+Eff44->GetYaxis()->SetRangeUser(0.,1.01);
+Eff44->Draw();
+MyC.Print("AllFourPlaneEfficiencies-v8noNoiseC.pdf");
+MyC.Print("AllFourPlaneEfficiencies-v8noNoiseC.png");
+MyC.Print("AllFourPlaneEfficiencies-v8noNoiseC.C");
+}
---
+++ EventFilter/test/zOthis6noise.cc
@@ -0,0 +1,74 @@
+{
+ TH1D* Eff24=new TH1D("Eff24",">=2/4 Plane efficiency in each wedge w/ 2sig noise cut;Wedge number;Efficiency",16,-0.5,15.5);
+ TH1D* Eff34=new TH1D("Eff34",">=3/4 Plane efficiency in each wedge w/ 2sig noise cut;Wedge number;Efficiency",16,-0.5,15.5);
+ TH1D* Eff44=new TH1D("Eff44","4/4 Plane efficiency in each wedge w/ 2sig noise cut;Wedge number;Efficiency",16,-0.5,15.5);
+const double pl3[16]={0.923943662,0.175347222,0.818181818,0.865030675,0.874145007,0.781055901,0.912547529,0.294656489,0.935828877,0.953383459,0.490467938,0.805642633,0.76744186,0.740554156,0.761811024,0.665749656};
+const double er3[16]={0.010005631,0.015856687,0.016796422,0.013407664,0.01229351,0.01629338,0.017629842,0.017796877,0.012848231,0.008275106,0.020775572,0.015670729,0.017861843,0.021977065,0.018890408,0.017477322};
+const double pl2[16]={0.926553672,0.89380531,0.874493927,0.822157434,0.,0.929759704,0.916030534,0.946078431,0.543478261,0.852150538,0.933993399,0.939670932,0.899371069,0.816666667,0.818181818,0.964143426};
+const double er2[16]={0.009863556,0.029528502,0.014951769,0.014608129,0.001527884,0.011080225,0.0173568,0.016319084,0.019598206,0.013029883,0.014498444,0.010286918,0.013846179,0.020412078,0.017747453,0.00848404};
+const double pl1[16]={0.798022599,0.946902655,0.710526316,0.922740525,0.98006135,0.946395564,0.916030534,0.779411765,0.894409938,0.911290323,0.851485149,0.92321755,0.94129979,0.905555556,0.877378436,0.93625498};
+const double er1[16]={0.015090456,0.022296185,0.020381256,0.010253197,0.005659628,0.009804384,0.0173568,0.029016378,0.01215246,0.010468017,0.02049186,0.01146705,0.010896899,0.015532691,0.015133285,0.0110185};
+const double pl0[16]={0.81884058,0.743055556,0.776548673,0.847389558,0.805800757,0.795031056,0.370942813,0.81122449,0.837209302,0.860406091,0.883561644,0.737226277,0.834572491,0.646825397,0.787476281,0.779436153};
+const double er0[16]={0.014670158,0.036314877,0.019587937,0.013172591,0.014050943,0.015908757,0.01896584,0.027987206,0.014088149,0.012363917,0.018887106,0.016806601,0.016037535,0.021255703,0.017819814,0.016882252};
+
+
+ for (int i=0;i<16;i++)
+ {
+	 const auto eff4=pl0[i]*pl1[i]*pl2[i]*pl3[i];
+	 const auto n0=1.-pl0[i];
+	 const auto n1=1.-pl1[i];
+	 const auto n2=1.-pl2[i];
+	 const auto n3=1.-pl3[i];
+	 auto eff3=n0*pl1[i]*pl2[i]*pl3[i];
+	 eff3+=pl0[i]*n1*pl2[i]*pl3[i];
+	 eff3+=pl0[i]*pl1[i]*n2*pl3[i];
+	 eff3+=pl0[i]*pl1[i]*pl2[i]*n3;
+	 auto eff2=n0*n1*pl2[i]*pl3[i];// 0011 + 1100 + 1010 + 1001 + 0101 + 0110
+	 eff2+=pl0[i]*pl1[i]*n2*n3;
+	 eff2+=pl0[i]*n1*pl2[i]*n3;
+	 eff2+=pl0[i]*n1*n2*pl3[i];
+	 eff2+=n0*pl1[i]*n2*pl3[i];
+	 eff2+=n0*pl1[i]*pl2[i]*n3;
+	 const auto e30=er0[i]*(pl1[i]*pl2[i]+pl1[i]*pl3[i]+pl2[i]*pl3[i]-3*pl1[i]*pl2[i]*pl3[i]);
+	 const auto e31=er1[i]*(pl0[i]*pl2[i]+pl0[i]*pl3[i]+pl2[i]*pl3[i]-3*pl0[i]*pl2[i]*pl3[i]);
+	 const auto e32=er2[i]*(pl0[i]*pl1[i]+pl0[i]*pl3[i]+pl1[i]*pl3[i]-3*pl0[i]*pl1[i]*pl3[i]);
+	 const auto e33=er3[i]*(pl0[i]*pl1[i]+pl0[i]*pl2[i]+pl1[i]*pl2[i]-3*pl0[i]*pl1[i]*pl2[i]);
+	 const auto erreff3=sqrt(e30*e30+e31*e31+e32*e32+e33*e33);
+	 const auto e40=er0[i]/pl0[i];
+	 const auto e41=er1[i]/pl1[i];
+	 const auto e42=er2[i]/(pl2[i]>0.000001 ? pl2[i] : 0.001);
+	 const auto e43=er3[i]/pl3[i];
+	 auto erreff4=eff4*sqrt(e40*e40+e41*e41+e42*e42+e43*e43);
+	 if (i==4) 
+		 erreff4=pl0[i]*pl1[i]*pl3[i]*er2[i];
+	 const auto e20=er0[i]*(3*pl1[i]*pl2[i]*pl3[i]+pl1[i]+pl2[i]+pl3[i]-2*pl1[i]*pl2[i]-2*pl1[i]*pl3[i]-2*pl2[i]*pl3[i]);
+	 const auto e21=er1[i]*(3*pl0[i]*pl2[i]*pl3[i]+pl0[i]+pl2[i]+pl3[i]-2*pl0[i]*pl2[i]-2*pl0[i]*pl3[i]-2*pl2[i]*pl3[i]);
+	 const auto e22=er2[i]*(3*pl0[i]*pl1[i]*pl3[i]+pl0[i]+pl1[i]+pl3[i]-2*pl0[i]*pl1[i]-2*pl0[i]*pl3[i]-2*pl1[i]*pl3[i]);
+	 const auto e23=er3[i]*(3*pl0[i]*pl1[i]*pl2[i]+pl0[i]+pl1[i]+pl2[i]-2*pl0[i]*pl1[i]-2*pl0[i]*pl2[i]-2*pl1[i]*pl2[i]);
+	 const auto erreff2=sqrt(e20*e20+e21*e21+e22*e22+e23*e23);
+	 Eff24->SetBinContent(i+1,eff2+eff3+eff4);
+	 Eff24->SetBinError(i+1,erreff2);
+	 Eff34->SetBinContent(i+1,eff3+eff4);
+	 Eff34->SetBinError(i+1,erreff3);
+	 Eff44->SetBinContent(i+1,eff4);
+	 Eff44->SetBinError(i+1,erreff4);
+ }
+
+TCanvas MyC;
+gStyle->SetOptStat(0);
+Eff24->GetYaxis()->SetRangeUser(0.,1.01);
+Eff24->Draw();
+MyC.Print("TwoOfFourPlaneEfficiencies-v9noiseC.pdf");
+MyC.Print("TwoOfFourPlaneEfficiencies-v9noiseC.png");
+MyC.Print("TwoOfFourPlaneEfficiencies-v9noiseC.C");
+Eff34->GetYaxis()->SetRangeUser(0.,1.01);
+Eff34->Draw();
+MyC.Print("ThreeOfFourPlaneEfficiencies-v9noiseC.pdf");
+MyC.Print("ThreeOfFourPlaneEfficiencies-v9noiseC.png");
+MyC.Print("ThreeOfFourPlaneEfficiencies-v9noiseC.C");
+Eff44->GetYaxis()->SetRangeUser(0.,1.01);
+Eff44->Draw();
+MyC.Print("AllFourPlaneEfficiencies-v9noiseC.pdf");
+MyC.Print("AllFourPlaneEfficiencies-v9noiseC.png");
+MyC.Print("AllFourPlaneEfficiencies-v9noiseC.C");
+}
