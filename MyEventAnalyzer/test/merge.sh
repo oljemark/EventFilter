971481ef35add366e969e69346ba4fec6a2854d8
Fredrik <fredrik.oljemark@helsinki.fi>
2023-11-20 10:50:32 +0100

    Combined 17.22MEvs

--- MyEventAnalyzer/test/merge.sh
+++ MyEventAnalyzer/test/merge.sh
@@ -1,2 +1,2 @@
-hadd ntuple_LBRT.root -f  test/dir_*/output_LBRT.root
-hadd ntuple_LTRB.root -f  test/dir_*/output_LTRB.root
+hadd ntuple_LBRT.root -f  nest/dir_*/output_LBRT.root
+hadd ntuple_LTRB.root -f  nest/dir_*/output_LTRB.root
